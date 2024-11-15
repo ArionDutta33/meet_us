a35a3d64bf93313bb9695109ff1e377cc485c9d2
ArionDutta33 <ariondutta333@outlook.com>
2024-11-15 21:11:59 +0530

    Initial setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { Link, Tabs } from 'expo-router';
 
 import { HeaderButton } from '../../components/HeaderButton';
@@ -7,13 +8,21 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: 'black',
+        tabBarActiveTintColor: 'orange', // Active tab color
+        tabBarInactiveTintColor: 'gray', // Inactive tab color
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: 'white',
+          marginBottom: 20,
+          marginHorizontal: 20,
+          borderRadius: 20,
+        },
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <AntDesign name="home" size={20} color={color} />, // Use the color prop here
           headerRight: () => (
             <Link href="/modal" asChild>
               <HeaderButton />
@@ -22,10 +31,17 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="create"
+        options={{
+          title: 'Tab Create',
+          tabBarIcon: ({ color }) => <AntDesign name="plus" size={20} color={color} />, // Use the color prop here
+        }}
+      />
+      <Tabs.Screen
         name="two"
         options={{
           title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="map" size={20} color={color} />, // Use the color prop here
         }}
       />
     </Tabs>
---
+++ app/index.tsx
@@ -0,0 +1,37 @@
+import { Link, Stack } from 'expo-router';
+import React from 'react';
+import { View, Text, Pressable, Image } from 'react-native';
+
+const OnBoardScreen = () => {
+  return (
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <View className="flex-1 bg-white">
+        <View className="  p-8">
+          <Text className="text-4xl font-semibold leading-snug tracking-widest">Welcome to </Text>
+          <Text className="text-4xl font-semibold leading-snug tracking-widest">Campus Meet</Text>
+        </View>
+        <View className="px-10">
+          <Text className="py-2 text-sm text-gray-500">Discoer your fellow students meetups </Text>
+          <Text className="text-sm text-gray-500">and create your own</Text>
+        </View>
+        <View className="mt-10 items-center ">
+          <Image
+            className="rounded-2xl"
+            source={{
+              uri: 'https://images.pexels.com/photos/3050833/pexels-photo-3050833.jpeg?auto=compress&cs=tinysrgb&w=300',
+            }}
+            style={{ width: 300, height: 400 }}
+          />
+        </View>
+        <Link href="/(tabs)" asChild>
+          <Pressable className="mx-6 mb-6 mt-auto rounded-xl bg-black py-4">
+            <Text className="text-center text-white">Get Started</Text>
+          </Pressable>
+        </Link>
+      </View>
+    </>
+  );
+};
+
+export default OnBoardScreen;
