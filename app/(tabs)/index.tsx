3dececd5831b6c87a69d7b6431401af97e239370
ArionDutta33 <ariondutta@333>
2024-11-18 23:55:30 +0530

    TS errors fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,59 @@
+import axios from 'axios';
 import { Link, router } from 'expo-router';
-import React from 'react';
-import { View, Text, FlatList, ScrollView, Pressable } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, ScrollView, Pressable, ToastAndroid } from 'react-native';
 
-import { events } from '~/assets/data';
 import EventItem from '~/components/EventItem';
-
+type Event = {
+  id: string;
+  name: string;
+  title: string;
+  description: string;
+  location: {
+    type: string;
+    coordinates: number[];
+  };
+  address: string;
+  date: string;
+  event_date: string;
+  max_attendees: number;
+  status: string;
+  cover_photo_url: string;
+  created_at: string;
+  updated_at: string;
+  photos: string[];
+};
 const FeedScreen = () => {
+  const [events, setEvents] = useState<Event[]>([]);
+  const getMeetUps = async () => {
+    try {
+      const response = await axios.get(`${process.env.EXPO_PUBLIC_API}/meetus`);
+      console.log('data log', JSON.stringify(response.data, null, 2));
+      // console.log(JSON.stringify(response, null, 2));
+      setEvents(response.data);
+      ToastAndroid.show('Events fetched successfully', ToastAndroid.SHORT);
+    } catch (error) {
+      console.log(JSON.stringify(error, null, 2));
+    }
+  };
+  useEffect(() => {
+    getMeetUps();
+  }, []);
+
   return (
-    <ScrollView showsVerticalScrollIndicator={false} className="m-4 flex-1 py-4">
-      {events.map((event) => {
-        return (
-          <Pressable key={event.id} onPress={() => router.push(`/details/${event.id}`)}>
-            <EventItem event={event} />
-          </Pressable>
-        );
-      })}
-    </ScrollView>
+    <View>
+      <FlatList
+        data={events}
+        renderItem={({ item }) => (
+          <Link href={`/details/${item.id}`} asChild>
+            <Pressable>
+              <EventItem event={item} />
+            </Pressable>
+          </Link>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
   );
 };
 
--- components/EventItem.tsx
+++ components/EventItem.tsx
@@ -1,22 +1,40 @@
+import moment from 'moment';
 import React from 'react';
-import { View, Text, Image, useWindowDimensions } from 'react-native';
-
-import { eventType } from '~/assets/data';
-const EventItem = ({ event }: { event: eventType }) => {
+import { View, Text, Image } from 'react-native';
+type Event = {
+  id: string;
+  name: string;
+  title: string;
+  description: string;
+  location: {
+    type: string;
+    coordinates: number[];
+  };
+  address: string;
+  date: string;
+  event_date: string;
+  max_attendees: number;
+  status: string;
+  cover_photo_url: string;
+  created_at: string;
+  updated_at: string;
+  photos: string[];
+};
+const EventItem = ({ event }: { event: Event }) => {
   return (
     <View className="m-4 overflow-hidden rounded-2xl border border-gray-300 ">
       <Image
         style={{ width: '100%', height: 200 }}
         resizeMode="cover"
         source={{
-          uri: event.image,
+          uri: event.photos[0],
         }}
       />
       <View className=" p-4">
         <Text className="text-xl font-bold">{event.title}</Text>
-        <Text className="font-semibold">{`${new Date(event.date).toDateString()}`}</Text>
+        <Text className="font-semibold">{`${moment(event.event_date).format('MMMM Do YYYY, h:mm a')}`}</Text>
         <View>
-          <Text className="fontmedium">{event.location.address}</Text>
+          <Text className="fontmedium">{event.address}</Text>
         </View>
       </View>
     </View>
